fba8c8988ff91ec6352b112e0569671a5ea41bd6
Radheshyam Soni <geekyradhe11@gmail.com>
2023-12-04 21:06:53 +0530

    feat: added :sparklses: Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Layout/Header";
+
 const App = () => {
-	return <div>Food Order App</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 };
 
 export default App;
