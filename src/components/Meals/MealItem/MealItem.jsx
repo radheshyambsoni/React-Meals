0e319edea08b99cde894616416f2a0e311ea628c
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-08 00:30:34 +0530

    feat: Added :construction: display items to cart logic

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -17,7 +17,7 @@ const MealItem = (props) => {
 	};
 
 	return (
-		<li className={classes.meal}>
+		<li key={props.id} className={classes.meal}>
 			<div>
 				<h3>{props.name}</h3>
 				<div className={classes.description}>{props.description}</div>
