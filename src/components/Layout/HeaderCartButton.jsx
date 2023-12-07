d3afa0ae15073945e025a35034cb8cebf039da43
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-07 23:45:20 +0530

    feat: Added :construction: add to cart logic using redcuer

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,21 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+	const cartCtx = useContext(CartContext);
+	const numOfCartItems = cartCtx.items.reduce((currNum, item) => {
+		return currNum + item.amount;
+	}, 0);
+
 	return (
 		<button className={classes.button} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{numOfCartItems}</span>
 		</button>
 	);
 };
